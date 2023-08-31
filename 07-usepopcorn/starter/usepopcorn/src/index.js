799e732fe80a402bb7cf95eb6d678970c3dcd863
Szabolcs Nagy <nagyszabolcs4455@gmail.com>
2023-08-31 09:25:25 +0200

    finish project usePopcorn

---
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} Stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color={"red"} className="test" defaultRating={3} />
+
+    <Test />
+  </React.StrictMode>
+);
