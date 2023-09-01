3166b1965b2e9cbba0911ca33fcfe6b0db5f6fa8
Szabolcs Nagy <tech.szabolcs@proton.me>
2023-09-01 21:49:01 +0200

    50% complete section 12

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App-v2";
 
 import StarRating from "./StarRating";
 
@@ -19,13 +19,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color={"red"} className="test" defaultRating={3} />
 
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
